1ae4445101066d01f057145701c682d64f22062e
Cícero <cicero.ferro.78@gmail.com>
2023-05-18 20:53:48 -0300

    Primeiro Commit

---
+++ src/main/java/com/br/frontendcicero/frontendcicero/FrontendciceroApplication.java
@@ -0,0 +1,13 @@
+package com.br.frontendcicero.frontendcicero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendciceroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontendciceroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/frontendcicero/frontendcicero/FrontendciceroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.frontendcicero.frontendcicero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontendciceroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
