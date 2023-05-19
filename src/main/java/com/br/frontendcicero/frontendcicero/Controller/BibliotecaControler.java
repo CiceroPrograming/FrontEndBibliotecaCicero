59a16865eceffc0f1c69d1e80383c14188bfd693
Cícero <cicero.ferro.78@gmail.com>
2023-05-19 19:45:30 -0300

    Login Corrigido

--- src/main/java/com/br/frontendcicero/frontendcicero/Controller/BibliotecaControler.java
+++ src/main/java/com/br/frontendcicero/frontendcicero/Controller/BibliotecaControler.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class BibliotecaControler {
 
-    @GetMapping("/home")
+    @GetMapping("/login")
     public String login() {
         return "login";
     }
